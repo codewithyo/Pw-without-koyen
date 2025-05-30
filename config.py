fb4ca53e3427310b0b96543d22fdf992b77fc876
codewithyo <102235878+codewithyo@users.noreply.github.com>
2025-05-30 18:57:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = "23634056"
 api_hash = "f2debf49c2f57bad88086ecd17cb5df3"
-bot_token = "7952974082:AAHRJPd0J6rdR_dTTMNU9g8VAzPXWmkDoRY"
+bot_token = "7838647960:AAFV_c63_j4Afs_IrH9B0QMVGYIczwGEzHk"
 auth_users = [5082207960,7612404009]
 owner_id = 5082207960  # Setting the owner ID
 admin_ids = [5082207960,7612404009]  # List of admin IDs
